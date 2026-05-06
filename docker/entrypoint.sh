79cef9a3e2bec952fd1c75e19b39c06e8e6e26af
vincelso <v.salotillo.554744@umindanao.edu.ph>
2026-05-06 20:36:53 +0800

    Run seeders on deploy

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-# Cache Laravel config
+# Clear old caches first
+php artisan config:clear
+php artisan cache:clear
+php artisan view:clear
+php artisan route:clear
+
+# Rebuild caches
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
@@ -8,6 +14,9 @@ php artisan storage:link
 
 # Run migrations
 php artisan migrate --force
+
+# Run seeders
+php artisan db:seed --force
 
 # Start PHP-FPM in background
 php-fpm -D
